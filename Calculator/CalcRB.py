e02e6efc883d10bd996c412dabc246a8f888539e
Robin Boldt <boldt@kuennecke.com>
2024-08-15 07:52:52 +0200

    Versuch Umsetzung benutzerdefinierte Rechnungmöglichkeiten

--- Calculator/CalcRB.py
+++ Calculator/CalcRB.py
@@ -21,30 +21,46 @@ class Rechnen:
         print(f"Ergebnis von {Za.x} * {Za.y} : {Za.x * Za.y}")
 
     def dividieren(Za):
-        print (f"Ergebnis von {Za.x} / {Za.y} : {Za.x / Za.y}")
+        try:
+            Ergebnis = Zahl[i] / Za.y
+        except:
+            n = Za.x / Za.y
+        if n == Za.x / Za.y
+            print(f"Ergebnis von {Za.x} / {Za.y} : {n}")
+        else:
+            print(f"Ergebnis von {n} / {Za.y} : {n}")
 
 def Zahleneingabe():
-        #x = input("\eine Zahl: ")
-        #Zahlen = []
-        #while '0' or '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' in x:
-            #try:
-                #x = float(x)
-                #Zahlen.append(x)
-                #x = input("eine Zahl?: ")
-                #print(Zahlen)
-            #except:
-                #break
+        x = input("\neine Zahl: ")
+        Zahlen = []
+        while '0' or '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' in x:
+            try:
+                x = float(x)
+                Zahlen.append(x)
+            except:
+                break
     
-        #global z
-        #z = Rechnen(Zahlen)
-        #for i in Zahlen:
-        #   b = b +-*/ Zahlen[i]
-        #   
-        x, y = float(input("\nerste Zahl: ")), float(input("zweite Zahl: "))
-        global z
-        z = Rechnen(x, y)
-
 aktiv = True
+
+def Rechenoperation():
+    global r_operationen
+    r_operationen = []
+    if len(r_operationen) > 0:
+        ro = input("Rechenoperation?")
+    else:
+        ro = input("""\nwelche Rechenoperation wilst du wählen? \n'+' fuer Addition \n'-' fuer Subtraktion \n'*' fuer Multiplikation
+'/' fuer Dividierung \noder irgendwas anderes zum abbrechen: """)
+    
+    if ro == '+':
+        r_operationen.append("+")
+    elif ro == '-':
+        r_operationen.append("-")
+    elif ro == '*':
+        r_operationen.append("*")
+    elif ro == '/':
+        r_operationen.append("/")
+    else:
+        return
 
 while aktiv:
     ro = input("""\nwelche Rechenoperation wilst du wählen? \n'1' fuer Addition \n'2' fuer Subtraktion \n'3' fuer Multiplikation
